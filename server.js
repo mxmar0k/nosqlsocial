2bca405bb9a3d2794b04a3a5c8c8da7df6ae1463
Hola Bart <marcoferrusquia@gmail.com>
2023-10-04 20:08:20 -0600

    update server js

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
 // this is for importing modules and other files
 const express = require('express');
-const db = require();
-const routes = require();
 
-// we initializing an instance of express
+// we have to import mongoose connection 
+const db = require('./config/connection');
+
+// we're importing the main router
+const routes = require('./routes');
+
+// We're initializing an instance of express
 const app = express();
 
-// this is the default port for our web server
-// process.env.PORT is usually for production environment
+// This is the default port for our web server.
+// process.env.PORT is usually for production environment (like Heroku) where the port is dynamically assigned.
 const PORT = process.env.PORT || 3001;
 
-// this is our middleware: 
-// express.json and express.urlencoded are built-in middleware functions for json and urlencoded daata
+// This is our middleware: 
+// express.json() and express.urlencoded() are built-in middleware functions in Express.
+// They parse incoming requests with JSON payloads and URL-encoded payloads, respectively.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// with this we tell our app to use the imported routes
+// With this, we tell our app to use the routes defined in the imported 'routes' module
 app.use(routes);
 
-// we have to connect to the MongoDB
-
+// Establishing connection to the MongoDB database
+// Once the database connection is established, our server starts listening for incoming requests.
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`🌍 API server running on port ${PORT}!`);
     });
-}).on('error', (error) => {
+})
+// Logging any database connection errors
+.on('error', (error) => {
     console.error(`🛑 Database connection error: ${error}`);
 });
-
