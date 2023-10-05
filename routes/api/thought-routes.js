bfab2693c9012aeb46913b0c8c97cf3c555c6aa0
Hola Bart <marcoferrusquia@gmail.com>
2023-10-04 20:06:23 -0600

    working on routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+// we import methods
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  // we can add more methods
+} = require('../../controllers/thought-controller');
+
+// we set up shop for the thought creation and getting
+router.route('/')
+  .get(getAllThoughts)      // we retrieve all thoughts from the database
+  .post(createThought);     // we create a new thought and add it to the database
+
+// then we set up dynamic routes with IDs for ops and thoughts 
+router.route('/:id')
+  .get(getThoughtById)      // we retrieve a specific thought by its id from the database
+  .put(updateThought)       // we update a specific thought's details by its id
+  .delete(deleteThought);   // we delete a specific thought by its id
+
+// we can add more routes if we want to
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+// we import methods 
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+  //  we can add any other controller methods 
+} = require('../../controllers/user-controller');
+
+// we have to set up root route to get all users and create a new one
+router.route('/')
+  .get(getAllUsers)      // retrieve all users from the database
+  .post(createUser);    // create a new user and add them to the database
+
+// then we set up dynamic routes with IDs for specific operations on individual users
+router.route('/:id')
+  .get(getUserById)     // we retrieve a specific user by their id from the database
+  .put(updateUser)      // or update a specific user details by their id
+  .delete(deleteUser);  // or delete a specific user by their id
+
+// we can add more routes as we see fit
+
+module.exports = router;
